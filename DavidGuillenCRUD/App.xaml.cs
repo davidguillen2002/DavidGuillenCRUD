edf344173f627c2f9aaa54b7b7ae7cc5cdc96f38
davidguillen2002 <david@LAPTOP-3KD5BQ0L>
2023-01-14 18:09:14 -0500

    Agregar archivos de proyecto.

---
+++ DavidGuillenCRUD/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DavidGuillenCRUD;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ DavidGuillenCRUD/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DavidGuillenCRUD;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
