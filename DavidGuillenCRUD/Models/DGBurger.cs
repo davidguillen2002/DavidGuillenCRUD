a41b298baa655c45a62e07e87c0a782b3ea92b5b
davidguillen2002 <david@LAPTOP-3KD5BQ0L>
2023-01-14 19:13:29 -0500

    Actualización DGBurger

---
+++ DavidGuillenCRUD/Models/DGBurger.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+
+namespace DavidGuillenCRUD.Models
+{
+    [Table("burger12345")]
+    public class DGBurger
+    {
+        [PrimaryKey, AutoIncrement]
+        public int DGId { get; set; }
+
+        [MaxLength(250)]
+        public string DGName { get; set; }
+        public string DGDescripcion { get; set; }
+        public bool DGWithExtraCheese { get; set; }
+    }
+}
