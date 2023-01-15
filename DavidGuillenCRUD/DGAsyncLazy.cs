f66c48376d2574d4e4231a74d33e071bdf4b8a03
davidguillen2002 <david@LAPTOP-3KD5BQ0L>
2023-01-14 19:14:16 -0500

    Clases para el control de eventos y BD

---
+++ DavidGuillenCRUD/DGAsyncLazy.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace DavidGuillenCRUD
+{
+    public class DGAsyncLazy<T>
+    {
+        readonly Lazy<Task<T>> instance;
+
+        public DGAsyncLazy(Func<T> factory)
+        {
+            instance = new Lazy<Task<T>>(() => Task.Run(factory));
+        }
+
+        public DGAsyncLazy(Func<Task<T>> factory) {
+            instance = new Lazy<Task<T>>(() => Task.Run(factory));
+        }
+         
+        public TaskAwaiter<T> GetAwaiter()
+        {
+            return instance.Value.GetAwaiter();
+        }
+
+        public void Start()
+        {
+            var unused = instance.Value;
+        }
+    }
+}
