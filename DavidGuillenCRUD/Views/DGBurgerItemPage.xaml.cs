6107408c88ce1828d9d5a64c58651f255bee50e6
davidguillen2002 <david@LAPTOP-3KD5BQ0L>
2023-01-14 19:13:01 -0500

    Cambios DGBurgerItemPage

---
+++ DavidGuillenCRUD/Views/DGBurgerItemPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using DavidGuillenCRUD.Data;
+using DavidGuillenCRUD.Models;
+using DavidGuillenCRUD.Views;
+
+namespace DavidGuillenCRUD.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DGBurgerItemPage : ContentPage
+    {
+        public DGBurgerItemPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveClicked(object sender, EventArgs e)
+        {
+            var todoItem = (DGBurger)BindingContext;
+            DGBurgerDatabase database = await DGBurgerDatabase.Instance;
+            await database.SaveItemAsync(todoItem);
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteClicked(object sender, EventArgs e)
+        {
+            var todoItem = (DGBurger)BindingContext;
+            DGBurgerDatabase database = await DGBurgerDatabase.Instance;
+            await database.DeleteItemAsync(todoItem);
+            await Navigation.PopAsync();
+        }
+
+        async void OnCancelClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
